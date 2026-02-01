0b58132934097c8d6554a8f23658652dce51d780
Argus <schwarzen221@gmail.com>
2026-02-01 22:26:20 +0800

    add navbar and sidebar

---
+++ client/src/app/(components)/sideabr.tsx
@@ -0,0 +1,5 @@
+const Sidebar = () => {
+  return <div className="flex">logo:Zionttsang</div>;
+};
+
+export default Sidebar;
--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -1,9 +1,18 @@
+import Navbar from "@/app/(components)/navbar";
+import Sidebar from "@/app/(components)/sideabr";
 
-
-const DashboardWrapper = ({children}: {children: React.ReactNode}) => {
+const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div>DashboardWrapper{children}</div>
-  )
-}
+    <div className="flex bg-gray-50 text-gray-900 w-full min-h-screen">
+      <Sidebar />
+      <main className="flex flex-col w-full h-full py-7 px-9 bg-gray-50 md:pl-24">
+        <Navbar />
+        {children}
+        <span>xxx</span>
+        <span>xxx</span>
+      </main>
+    </div>
+  );
+};
 
-export default DashboardWrapper
+export default DashboardWrapper;
